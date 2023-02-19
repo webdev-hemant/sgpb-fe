26afa9bd16f79c1b88b3855034d5cab6a35e066b
Hemant Jadhav <webdev.hemant@example.com>
2023-02-19 19:14:38 +0530

    setup complete

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>||श्री गुरुकृपा पूजा भंडार||</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="||श्री गुरुकृपा पूजा भंडार||" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+    </>
+  );
+}
